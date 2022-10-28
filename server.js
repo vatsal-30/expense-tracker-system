c4d6decf827fef67f393af9b4da8d0ec812b8036
vatsal-30 <vatsalajmeri30802@gmail.com>
2022-10-28 17:13:46 +0530

    fourth commit

--- server.js
+++ server.js
@@ -30,4 +30,3 @@ if(process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
-
